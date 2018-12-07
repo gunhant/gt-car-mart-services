2193936511492a1fab235370e06612d72d3fecb5
Gunhan Tatman <gunhantatman@hotmail.com>
2018-12-07 16:43:56 -0500

    Initial commit

---
+++ src/main/java/com/tatman/gtcarmartservices/GtCarMartServicesApplication.java
@@ -0,0 +1,12 @@
+package com.tatman.gtcarmartservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtCarMartServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtCarMartServicesApplication.class, args);
+	}
+}
