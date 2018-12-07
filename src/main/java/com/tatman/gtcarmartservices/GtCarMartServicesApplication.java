1af63ec06b2061f91bcd06b38c3601b39e9a2f6d
Gunhan Tatman <gunhantatman@hotmail.com>
2018-12-07 16:46:10 -0500

    Started the services project on 12-7-2018

--- src/main/java/com/tatman/gtcarmartservices/GtCarMartServicesApplication.java
+++ src/main/java/com/tatman/gtcarmartservices/GtCarMartServicesApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GtCarMartServicesApplication {
 
+	// Started on 12-7-2018
+
 	public static void main(String[] args) {
 		SpringApplication.run(GtCarMartServicesApplication.class, args);
 	}
