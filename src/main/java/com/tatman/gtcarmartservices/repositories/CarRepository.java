865724e8bcccc9f4f0d13f9112bd797a3cd96cf5
Gunhan Tatman <gunhantatman@hotmail.com>
2018-12-07 17:05:46 -0500

    Added CarRepository

---
+++ src/main/java/com/tatman/gtcarmartservices/repositories/CarRepository.java
@@ -0,0 +1,9 @@
+package com.tatman.gtcarmartservices.repositories;
+
+import com.tatman.gtcarmartservices.entities.Car;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository<Car, Long> {
+
+
+}
