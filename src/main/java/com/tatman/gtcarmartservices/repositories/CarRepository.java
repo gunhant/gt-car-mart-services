ea1e63f73d4382cedd8690d8753e5dd2fa828c8d
Gunhan Tatman <gunhantatman@hotmail.com>
2018-12-09 18:09:11 -0500

    Added @RepositoryRestResource to CarRepository to enable searches with custom methods such as FindByColor

--- src/main/java/com/tatman/gtcarmartservices/repositories/CarRepository.java
+++ src/main/java/com/tatman/gtcarmartservices/repositories/CarRepository.java
@@ -2,6 +2,8 @@ package com.tatman.gtcarmartservices.repositories;
 
 import com.tatman.gtcarmartservices.entities.Car;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
@@ -18,10 +20,11 @@ void deleteAll()	            Deletes all entities of the repository
 
  */
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
 
-    List<Car> findByBrand(String brand);
-    List<Car> findByColor(String color);
-    List<Car> findByYear(int year);
+    List<Car> findByBrand(@Param("brand") String brand);
+    List<Car> findByColor(@Param("color") String color);
+    List<Car> findByYear(@Param("year") int year);
 
 }
